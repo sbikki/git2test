64f3b0b4a6a8fc505891b7757309ef2ca34434a4
Srinivas Bikki <sbikkibd@gmail.com>
2018-04-15 17:01:26 -0700

    add .gitignore file, modified calc.py file

--- calc.py
+++ calc.py
@@ -1,3 +1,7 @@
 Add a new line for cloned repository
 Add a test line for create branch
+<<<<<<< HEAD
 Add a fast example
+=======
+Add a Undo Line
+>>>>>>> 92b3cc9... Completed Other  Undo Line Update
