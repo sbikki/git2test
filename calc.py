482dd29fadfcc81b2865795679bd2f3e235c1834
Srinivas Bikki <sbikkibd@gmail.com>
2018-04-16 06:25:47 -0700

    add

--- calc.py
+++ calc.py
@@ -4,4 +4,8 @@ Add a test line for create branch
 Add a fast example
 =======
 Add a Undo Line
+<<<<<<< HEAD
 >>>>>>> 92b3cc9... Completed Other  Undo Line Update
+=======
+2222
+>>>>>>> aaca23c... Completed the 2222 Line
